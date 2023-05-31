f6bf7f98827c55ef02a1c44e41434e1c5eaabc9a
KT <132851569+kumih0@users.noreply.github.com>
2023-05-31 14:33:21 -0500

    adding pseudo-code for tomtom

---
+++ assets/script/tomtom-notes.js
@@ -0,0 +1,20 @@
+// Here are some notes for tomtomapi
+
+// category ids to filter relevant search results during nearby location fetch
+//mini-project reference dropdown form> when selected, category id will be plugged into fetch request url (&categoryset=numbers)
+// "id": 7315, "name": "Restaurant", 
+// "id": 7383,"name": "Airport",
+//"id": 9379,"name": "Nightlife"
+//"id": 7314,"name": "Hotel/Motel"
+//"id": 9927,"name": "Zoo, Arboretum & Botanical Garden",
+// "id": 7376, "name": "Tourist Attraction",
+// "id": 7374,"name": "Stadium",
+
+//pseudo-code:
+//
+//save btn - add save local storage event of fave places.
+//basic info for each result name/phone/address/category/url
+//address url links, location (address), categories-relevant data hotels, food, 
+//append data to card in html
+//dynamically create card within set limit in fetch req data obj output
+//
