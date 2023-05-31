1becd2dbbf7a711bd088677e5c17393ac91c0582
david-leverenz <david.leverenz@gmail.com>
2023-05-31 13:23:06 -0500

    pulling data for correct day

--- assets/script/script.js
+++ assets/script/script.js
@@ -75,111 +75,124 @@ var cityName = "string";
 
 // This function inserts the city into the apiURL then fetches the latitude and longitude of the city.  It checks the cities in the storage array and if the current searched city is not in the array, it adds it to the array.  If it's already in there it doesn't re-add the city to the array.  Then it runs the getWeather and getFiveDay functions.
 
-// var getLatLonCity = function (city) {
+var getLatLonCity = function (city) {
 
-//     var apiURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=44be570f60fd1ef1f012456a39e5a0ff";
+    var apiURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=44be570f60fd1ef1f012456a39e5a0ff";
 
-//     fetch(apiURL).then(function (response) {
+    fetch(apiURL).then(function (response) {
 
-//         if (response.ok) {
-//             response.json().then(function (data) {
-//                 for (var i = 0; i < data.length; i++) {
-//                     latitude = data[i].lat;
-//                     longitude = data[i].lon;
-//                     cityName = data[i].name;
+        if (response.ok) {
+            response.json().then(function (data) {
+                for (var i = 0; i < data.length; i++) {
+                    latitude = data[i].lat;
+                    longitude = data[i].lon;
+                    cityName = data[i].name;
 
-//                     if (!citiesInStorage.includes(cityName)) {
-//                         citiesInStorage.push(cityName);
-//                         localStorage.setItem("citySearch", JSON.stringify(citiesInStorage));
-//                     }
+                    // if (!citiesInStorage.includes(cityName)) {
+                    //     citiesInStorage.push(cityName);
+                    //     localStorage.setItem("citySearch", JSON.stringify(citiesInStorage));
+                    // }
 
-//                     getForecast(latitude, longitude);
-//                 }
-//             })
-//         }
-//     })
-// };
+                    console.log(latitude, longitude, cityName)
 
+                    getForecast(latitude, longitude);
+                }
+            })
+        }
+    })
+};
 
-var displayDay = dayjs("6-14-2023").format("MM-DD-YYYY");
+getLatLonCity("Paris");
+
+var displayDay = dayjs("6-14-2023").format("M-D-YYYY");
 var inputDay = dayjs("6-14-2023");
 
-console.log(inputDay);
-console.log(displayDay);
+// console.log(inputDay);
+// console.log(displayDay);
 
 // Get the current day and format it the way I want.
 
 // dayjs.extend(customParseFormat);
 var today = dayjs().format("M-D-YYYY");
 
-console.log(today);
+// console.log(today);
+
 
 var determineArrayPosition = function () {
     if (inputDay.diff(today,"day")>30) {
-        alert("We can't that far in the future, please select a day no more than 30 days in the future.");
+        alert("We can't see that far into the future.  Please select a day no more than 30 days into the future.");
         return;
     } else if (inputDay.diff(today,"day")<=30) {
-        var i = inputDay.diff(today,"day")
+        var arrayPosition = inputDay.diff(today,"day");
+        return arrayPosition;
 
-        console.log("Made it to where i should display");
-        console.log(i);
+        // console.log("Made it to where i should display");
+        // console.log(i);
     }
 }
 
-determineArrayPosition();
+// determineArrayPosition();
 
 
-// var getForecast = function (latitude, longitude) {
-//     var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=44be570f60fd1ef1f012456a39e5a0ff";
+var getForecast = function (latitude, longitude) {
+    var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=44be570f60fd1ef1f012456a39e5a0ff";
 
 
-//     fetch(fiveDayURL).then(function (response) {
-//         response.json().then(function (fiveDayData) {
+    fetch(fiveDayURL).then(function (response) {
+        response.json().then(function (forecastData) {
 
-//             // i is the arrayPosition calculated from inputDay-today
+            console.log(forecastData);
 
-//             rowDivEl.innerHTML = "";
+            var dayChosen = determineArrayPosition();
+
+            // i is the arrayPosition calculated from inputDay-today
+
+            // rowDivEl.innerHTML = "";
+
+            console.log(dayChosen);
            
-//                 var foreDay = dayjs().add([i], "day").format("M/D/YYYY");
-//                 var foreIcon = fiveDayData.list[i].weather[0].icon;
-//                 var foreTemp = ((((fiveDayData.list[i].main.temp) - 273.15) * 1.8) + 32).toFixed(2) + " F";
-//                 var foreWind = fiveDayData.list[i].wind.speed;
-//                 var foreHumidity = fiveDayData.list[i].main.humidity;
+                var foreDay = dayjs().add([dayChosen], "day").format("M/D/YYYY");
+                var foreIcon = forecastData.list[dayChosen].weather[0].icon;
+                var foreTemp = ((((forecastData.list[dayChosen].main.temp) - 273.15) * 1.8) + 32).toFixed(2) + " F";
+                var foreWind = forecastData.list[dayChosen].wind.speed;
+                var foreHumidity = forecastData.list[dayChosen].main.humidity;
 
-//                 var foreList = document.createElement("div");
-//                 foreList.setAttribute("class", "col-12 col-xl");
-//                 var cardEl = document.createElement("div");
-//                 cardEl.setAttribute("class", "card p-3 my-2 fs-6");
-//                 var titleEl = document.createElement("h4");
-//                 var tempEl = document.createElement("p");
-//                 var windEl = document.createElement("p");
-//                 var humidityEl = document.createElement("p");
-//                 titleEl.textContent = foreDay;
-//                 tempEl.textContent = "TEMP: " + foreTemp;
-//                 windEl.textContent = "WIND: " + foreWind + " MPH";
-//                 humidityEl.textContent = "HUMIDITY: " + foreHumidity + "%";
+                console.log("Day: " + foreDay + ", Icon: " +foreIcon+ ", Temp: " + foreTemp + ", Wind: " +foreWind+ ", Humidity: " + foreHumidity);
 
-//                 var weatherPicture = "https://openweathermap.org/img/w/" + foreIcon + ".png";
+                var foreList = document.createElement("div");
+                foreList.setAttribute("class", "col-12 col-xl");
+                var cardEl = document.createElement("div");
+                cardEl.setAttribute("class", "card p-3 my-2 fs-6");
+                var titleEl = document.createElement("h4");
+                var tempEl = document.createElement("p");
+                var windEl = document.createElement("p");
+                var humidityEl = document.createElement("p");
+                titleEl.textContent = foreDay;
+                tempEl.textContent = "TEMP: " + foreTemp;
+                windEl.textContent = "WIND: " + foreWind + " MPH";
+                humidityEl.textContent = "HUMIDITY: " + foreHumidity + "%";
+
+                var weatherPicture = "https://openweathermap.org/img/w/" + foreIcon + ".png";
 
 
-//                 var pic = document.createElement("img");
-//                 pic.setAttribute("alt", "weather icon");
-//                 pic.src = weatherPicture;
-//                 pic.setAttribute("height", "50");
-//                 pic.setAttribute("width", "50");
+                // var pic = document.createElement("img");
+                // pic.setAttribute("alt", "weather icon");
+                // pic.src = weatherPicture;
+                // pic.setAttribute("height", "50");
+                // pic.setAttribute("width", "50");
 
-//                 cardEl.appendChild(titleEl);
-//                 cardEl.appendChild(pic);
+                // cardEl.appendChild(titleEl);
+                // cardEl.appendChild(pic);
 
-//                 cardEl.appendChild(tempEl);
-//                 cardEl.appendChild(humidityEl);
-//                 cardEl.appendChild(windEl);
-//                 foreList.appendChild(cardEl);
-//                 rowDivEl.appendChild(foreList);
+                // cardEl.appendChild(tempEl);
+                // cardEl.appendChild(humidityEl);
+                // cardEl.appendChild(windEl);
+                // foreList.appendChild(cardEl);
+                // rowDivEl.appendChild(foreList);
            
-//         })
-//     });
-// }
+        })
+    });
+}
 
 // This adds event listeners on my buttons so that they do something.
 
